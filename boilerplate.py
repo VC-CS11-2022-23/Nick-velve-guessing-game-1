93123d4c7f21f6ae671ed292340dc697c2963aec
WeaponisedDog <119447536+WeaponisedDog@users.noreply.github.com>
2022-12-05 21:22:53 +0000

    hehe

--- boilerplate.py
+++ boilerplate.py
@@ -1,15 +1,15 @@
-"""SCRIPT"""
-
-# BOILERPLATE
-import random
-
-# this generates a random number between 1 and 10
-number = random.randint(1, 10)
-
 TRIES = 0
-
-# GAME LOOP
-while True:
+import random
+number = random.randint(1,10)
+while TRIES < 3:
     guess = int(input("Guess a number between 1 and 10: "))
-
-    # implement your logic below
+    TRIES = TRIES + 1
+    if guess == number:
+        print("correct")
+        break
+    if guess > number:
+        print("too high")
+    else:
+        print("too low")
+if TRIES == 3:
+    print("That was your last try! ")
